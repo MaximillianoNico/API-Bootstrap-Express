7c628c298484dc292a2e312b96744dbced30db92
MaximillianoNico <maximillianonico8@gmail.com>
2020-03-09 18:24:02 +0700

    set default app

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const mongo = require('./config/mongo');
 const mysql = require('./config/mysql');
 const server = require('./lib/server');
+const express = require('express');
+const defApp = express();
 
 module.exports ={
 
-    start: function(app,loader,port){
+    start: function(app = defApp,loader,port){
         return new Promise((resolve,reject)=>{
             server.start(app,loader,port).then((result) => {
                 resolve(result);
